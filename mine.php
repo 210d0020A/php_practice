d29132093da8e32ef3d7948c33f2a0623fb13dfe
m-ajioka <ajiokamine@gmail.com>
2023-02-07 07:37:59 +0000

    mine.php

---
+++ mine.php
@@ -0,0 +1,15 @@
+<?php
+echo 'hello php!';
+
+$integar = 20;
+$string = "abc";
+echo $integar;
+echo $string;
+
+$mine = 100;
+$new_mine = $mine + 10;
+echo $new_mine;
+
+$array = [2017, 2018, 2019, 2020];
+echo $array[2];
+
---
+++ phptask06.php
@@ -0,0 +1,18 @@
+<?php
+function twice($n){
+ //数値を2倍にする
+ $result = $n * 2;
+ return $result;
+ }
+ 
+ echo twice(6);
+ 
+ function add($a, $b){
+ //処理したい内容
+ $result = $a + $b;
+ return $result;
+ }
+ echo "\n";
+ echo add(1, 2);
+ echo "\n";
+ 
